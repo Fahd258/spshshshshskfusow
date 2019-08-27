ba84fa1ce9807bd01c875bbfc617f266ce49b1dd
Fahd258 <54243960+Fahd258@users.noreply.github.com>
2019-08-27 17:27:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -512,7 +512,7 @@ client.on('message', async(message) => {
 
 
 
-var prefix = '!!'; // your prefix
+var prefix = '!!'; 
 client.on('message', message => {
   if(message.content.split(' ')[0] == `${prefix}ban`){
   if(!message.guild || message.author.bot) return undefined;
