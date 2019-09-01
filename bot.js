3681eb01624f8853bfcd2ca6e22fb33e3154aba6
Fahd258 <54243960+Fahd258@users.noreply.github.com>
2019-09-01 20:23:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -570,6 +570,26 @@ if (message.member.voiceChannel == null) return;
 
 
 
+client.on('message',zaid => {
+   if(zaid.content === prefix + "closeroom") {
+   if(!zaid.channel.guild) return zaid.channel.send('**This command is only done on servers**');
+   if(!zaid.member.hasPermission('MANAGE_MESSAGES')) return zaid.channel.send('**:x: - No Permissions ! **');
+    zaid.channel.overwritePermissions(zaid.guild.id, {
+    SEND_MESSAGES: false
+    }).then(() => {
+    zaid.channel.send("**:white_check_mark: | Channel Closed :lock:**")
+});
+   }
+   if(zaid.content === prefix + "openroom") {
+   if(!zaid.channel.guild) return zaid.channel.send('**This command is only done on servers**');
+   if(!zaid.member.hasPermission('MANAGE_MESSAGES')) return zaid.channel.send(**:x: - No Permissions ! **');
+    zaid.channel.overwritePermissions(zaid.guild.id, {
+    SEND_MESSAGES: true
+    }).then(() => {
+    zaid.channel.send("**:white_check_mark: | Channel Opened :unlock:**")
+ });
+ }      
+});
 
 
 
