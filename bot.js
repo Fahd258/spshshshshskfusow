0c448f1b12fe097e07ef7f034fc01a36346a49f8
Fahd258 <54243960+Fahd258@users.noreply.github.com>
2019-09-02 18:25:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1045,4 +1045,96 @@ client.on('message', (message) => {
 
 
 
+
+
+const Captcha = JSON.parse(fs.readFileSync("./Captcha.json","utf8"));
+client.on("message", msg => {
+    if(!msg.guild) return;
+    if(msg.author.bot) return;
+    if(!Captcha[msg.guild.id]) Captcha[msg.guild.id] = {
+        role: "Nothing",
+        room: "Nothing",
+        cmd: "Captcha"
+    }
+    if(msg.content.startsWith(prefix + "setCaptcharole")){
+                if(!msg.guild.member(msg.author).hasPermission('MANAGE_GUILD')) return message.reply(`**Sorry But You Don\'t Have Permission \`MANAGE_GUILD\`**`).then(m => m.delete(5000));
+        let args = msg.content.split(' ').slice(1).join(' ');
+        if(!args) return msg.reply(`**عليك كتابت اسم الرتبة الخاصة بالتفعيل**`).then(m => m.delete(5000));
+        if(!msg.guild.roles.find("name",args)) return msg.channel.send(`لا يوجد رتبة بأسم \`${args}\` `).then(m => m.delete(5000));
+        Captcha[msg.guild.id].role = args
+  msg.reply(`**تم تغير اسم الرتبة الخاصة بالتفعيل لـ \`${args}\`**`).then(m => m.delete(5000));
+                    fs.writeFile("./Captcha.json", JSON.stringify(Captcha), function(a) {
+        if (a) throw a;
+    })
+    }
+    if(msg.content.startsWith(prefix + "setCaptcharoom")){
+if(!msg.guild.member(msg.author).hasPermission('MANAGE_GUILD')) return message.channel.send(`**Sorry But You Don\'t Have Permission \`MANAGE_GUILD\`**`).then(m => m.delete(5000));
+         let args2 = msg.content.split(' ').slice(1).join(' ');
+        if(!args2) return msg.reply(`**عليك كتابت اسم الرتبة الخاصة بالتفعيل**`).then(m => m.delete(5000));
+         if(!msg.guild.channels.find("name",args2)) return msg.channel.send(`**لا يوجد روم بأسم \`${args2}\`**`).then(m => m.delete(5000));
+        Captcha[msg.guild.id].room = args2
+  msg.reply(`**تم تغير اسم الروم الخاص بالتفعيل لـ \`${args2}\`**`).then(m => m.delete(5000));
+            fs.writeFile("./Captcha.json", JSON.stringify(Captcha), function(a) {
+        if (a) throw a;
+    })
+    }
+      if(msg.content.startsWith(prefix + "setCaptchacmd")){
+if(!msg.guild.member(msg.author).hasPermission('MANAGE_GUILD')) return message.channel.send(`**Sorry But You Don\'t Have Permission \`MANAGE_GUILD\`**`).then(m => m.delete(5000));
+         let args3 = msg.content.split(' ').slice(1).join(' ');
+        if(!args3) return msg.reply(`**عليك كتابت الامر الخاص بالتفعيل**`).then(m => m.delete(5000));
+ 
+        Captcha[msg.guild.id].cmd = args3
+  msg.reply(`**تم تغير امر الخاص بالتفعيل لـ \`${args3}\` **`).then(m => m.delete(5000));
+            fs.writeFile("./Captcha.json", JSON.stringify(Captcha), function(a) {
+        if (a) throw a;
+    })
+      }
+});
+ 
+        client.on("message",async message => {
+        if(!message.channel.guild) return;
+        if(!Captcha[message.guild.id]) Captcha[message.guild.id] = {
+        role: "Nothing",
+        room: "Nothing",
+        cmd: "Captcha"
+        }
+        if(message.content.startsWith(prefix + Captcha[message.guild.id].cmd || "Captcha")){
+        if(Captcha[message.guild.id].role === 'Nothing') return message.reply(`**لم يتم اختيار الرتبة الخاصة بالتفعيل عليك ان تكتب \`${prefix}setCaptcharole\`**`).then(m => m.delete(5000));
+        if(Captcha[message.guild.id].room === 'Nothing') return message.reply(`**لم يتم اختيار الروم الخاص بالتفعيل عليك ان تكتب \`${prefix}setCaptcharoom\`**`).then(m => m.delete(5000));
+        if (message.guild.member(message.author).roles.find(x => x.name === `${Captcha[message.guild.id].role}`)) return message.channel.send(`**${message.author}, انت تملك الرتبة بالفعل**`).then(m => m.delete(5000));
+        if(!message.channel.guild) return message.channel.send(`**هذا الأمر فقط للسيرفرات**`).then(m => m.delete(5000));
+        if(!message.guild.member(client.user).hasPermission('MANAGE_ROLES')) return message.channel.send(`**Sorry But I Don\'t Have Permission \`MANAGE_GUILD\`**`).then(m => m.delete(5000));
+        if(message.channel.name !== `${Captcha[message.guild.id].room}`) return message.reply(`**انت لست في الروم الصحيح برجاء التوجه الي روم \`${Captcha[message.guild.id].room}\`**`).then(m => m.delete(5000));
+        const canvas = Canvas.createCanvas(108 , 40);
+        const ctx = canvas.getContext('2d');
+        const background = await Canvas.loadImage("https://cdn.discordapp.com/attachments/608278049091223552/617791172810899456/hmmm.png");
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);  
+        var one = Math.floor(Math.random() * 9) + 1;
+        var two = Math.floor(Math.random() * 9) + 1;
+        var three = Math.floor(Math.random() * 9) + 1;
+        var four = Math.floor(Math.random() * 9) + 1;
+        var number = `${one}${two}${three}${four}`;
+        ctx.font = '20px Arial Bold';
+        ctx.fontSize = '20px';
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(number, canvas.width / 2.4, canvas.height / 1.7);
+        const attachment = new Discord.Attachment(canvas.toBuffer());
+        message.channel.send(attachment).then(m => {
+        message.channel.awaitMessages(m => m.author.id === message.author.id, {max: 1, time: 60000}).then(c => {
+        if(c.first().content !== number) {
+        m.delete();
+        }else if(c.first().content === number) {
+        m.delete();
+        message.member.addRole(message.guild.roles.find("name", Captcha[message.guild.id].role));    
+        message.channel.send(`**${message.author.username}, تم تفعيلك بنجاح**`).then(m => m.delete(1000));
+        }
+        })
+        });
+        }
+        });
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
