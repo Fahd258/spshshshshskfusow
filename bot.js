69dd41db4c8bea40c3b49317a8d7405944b9a6ba
Fahd258 <54243960+Fahd258@users.noreply.github.com>
2019-09-03 17:53:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1034,40 +1034,30 @@ My ID : ${client.user.id}\`\`\`**`)
 
 
 
+client.on('message', msg => {
+  if(msg.content === '!!hide') {
+    msg.guild.channels.forEach(c => {
+      c.overwritePermissions(msg.guild.id, {
+        SEND_MESSAGES: false,
+        READ_MESSAGES: false
+      })
+    })
+    msg.channel.send('تم اخفاء الرومات من الاعضاء')
+  }
+})
 
 
-
-
-
-client.on("message", message => {
-    var gs = require('github-scraper');
-     let args = message.content.split(" ").slice(1).join(" ")
-            if(message.content.startsWith(prefix + "github")) {
-        if(!args) return message.channel.send("Type The Username To Scrap The Informations")
-  gs(args, function(err, data) {
-    let embed = new Discord.RichEmbed()
-        .setAuthor(data.username, data.avatar)
-        .setTitle(`${args} Profile:`)
-        .setURL(`https://github.com/${args}`)
-        .setThumbnail(data.avatar)
-        .setDescription(`User Description: ${data.bio}`)
-        .addField("Username:", `${data.name}`)
-        .addField("User ID:", `${data.uid}`)
-        .addField("Repositorys:", `${data.repos}`)
-        .addField("Projects:", `${data.projects}`)
-        .addField("Stars:", `${data.stars}`)
-        .addField("Followers:", `${data.followers}`)
-        .addField("Worksfor:", `${data.worksfor}`)
-        .addField("Location:", `${data.location}`)
-        .addField("Website:", `${data.website}`)
-        .setColor("BLACK")
-        .setFooter(message.author.tag, message.author.avatarURL)
-        message.channel.sendEmbed(embed)
-        if(err) return console.log(err)        
-
-        })
-    }
-});
+client.on('message', msg => {
+  if(msg.content === '!!unhide') {
+    msg.guild.channels.forEach(c => {
+      c.overwritePermissions(msg.guild.id, {
+        SEND_MESSAGES: true,
+        READ_MESSAGES: true
+      })
+    })
+    msg.channel.send('تم اظهار الرومات كلها للاعضاء')
+  }
+})
 
 
 client.login(process.env.BOT_TOKEN);
