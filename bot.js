3c25cfe87627f874664dec8ecc1f24c6dbe518fe
Fahd258 <54243960+Fahd258@users.noreply.github.com>
2019-11-03 21:22:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1187,4 +1187,26 @@ client.on('message', message => {
     }
 });
 
+
+lient.on("message", (message) => {
+  if (message.channel.type == "dm" && message.author.id != client.user.id) {
+    let owner = client.users.get("435734273299841024");
+    let channel = client.channels.get("CHANNEL ID"); // ذي لو تبيه ب روم
+    if (owner) {
+      let embed = new RichEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setTitle(`Message sent by ${message.author.username}`)
+        .setTimestamp(message.author.avatarURL)
+        .setDescription(message.content)
+        .setFooter(client.user.username, client.user.avatarURL)
+        .setTimestamp();
+      try {
+        owner.send(embed);
+      } catch (e) {
+        console.log(e);
+      }
+    } else console.log("user not found.");
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
